9a4e80df910bf25d126f2edddba769df71581e8e
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-10-19 18:32:10 +0919

    ans: easy-11

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,7 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P
+}
+
+// MEMO
+// PropertyKey = string | number | symbol
+// というオブジェクトのKeyの取りうる値の型がある
