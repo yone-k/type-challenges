d994a9122509b15ddb01259c728605840e080c32
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-10-21 23:54:29 +0919

    ans: medium-2

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,2 @@
-type MyReturnType<T> = any
+// 関数型Tが(...args: any[]) => infer Rの形式に一致するかを条件型で判定し、一致する場合は推論された戻り値の型Rを返す。一致しない場合はneverを返す。
+type MyReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : never
