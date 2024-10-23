b879dad20ad88991e49278ff1dc9132352217850
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-10-23 18:53:18 +0919

    ans: medium-3

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,4 @@
-type MyOmit<T, K> = any
+// マップド型を使用してTの各プロパティをイテレートし、キーがKに含まれる場合はneverとして除外し、含まれない場合はそのキーを保持する
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
