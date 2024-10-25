cc499c59164ff51b22861240dfa7a8599d01c1c9
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-10-25 21:10:50 +0919

    ans: medium-8

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,6 @@
-type MyReadonly2<T, K> = any
+// 指定されたプロパティを読み取り専用にする型。交差型を使用して、Kに指定されたプロパティは読み取り専用にし、それ以外のプロパティはそのままの型で保持する。Kが指定されない場合はデフォルトですべてのプロパティを読み取り専用にする。
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
