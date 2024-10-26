52de86ce808cdfd243c589d28ac6cacb7434dba4
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-10-26 21:01:55 +0919

    ans: medium-9

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,5 @@
-type DeepReadonly<T> = any
+// 型を再帰的に読み取り専用にする型。オブジェクト型の場合は各プロパティに対してDeepReadonlyを再帰的に適用するが、関数型の場合はそのまま返す。
+type DeepReadonly<T> = T extends object ?
+  T extends Function ? T : {
+    readonly [K in keyof T]: DeepReadonly<T[K]>
+  } : T
