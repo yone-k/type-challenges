c707b28d807b48433442d583ddcf70578a42f47f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-10-30 23:09:40 +0919

    ans: medium-12

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,6 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+// メソッドチェーンでオプションを設定できる型を実装。ジェネリックパラメータTで現在の状態を保持し、option呼び出しごとにTに新しいプロパティを追加する。
+// 同じキーが2回使用されないように、キーがすでにTに存在する場合はnever型となるように制約する。
+type Chainable<T = {}> = {
+  option<K extends string, V>(key: K extends keyof T ? never : K, value: V): Chainable<Omit<T, K> & { [P in K]: V }>
+  get(): T
 }
