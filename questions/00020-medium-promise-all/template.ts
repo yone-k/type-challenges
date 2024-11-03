1f3694392df3a7da7f879f5cfd590e598cfdff69
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-03 15:36:10 +0919

    ans: medium-20

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,2 @@
-declare function PromiseAll(values: any): any
+// Promise.all関数の型定義。入力配列の各要素がPromiseの場合はその解決値を、Promiseでない場合はそのまま値を持つ配列を返すPromiseを表現。
+declare function PromiseAll<T extends unknown[]>(values: readonly [...T]): Promise<{ [K in keyof T]: Awaited<T[K]> }>
