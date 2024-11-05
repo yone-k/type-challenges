9bb917b293ed8efec63ce54cf5766185c99d58f3
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-05 19:08:56 +0919

    ans: medium-106

--- questions/00106-medium-trimleft/template.ts
+++ questions/00106-medium-trimleft/template.ts
@@ -1,1 +1,3 @@
-type TrimLeft<S extends string> = any
+// 文字列型の左側の空白文字（スペース、改行、タブ）を削除する。テンプレートリテラル型と条件型、再帰を組み合わせて実装。
+// 文字列の先頭が空白文字の場合、その部分を除いた残りの文字列にTrimLeftを再帰的に適用する。
+type TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer R}` ? TrimLeft<R> : S
