4b44c38e67742f1eea05118fe77eaef3e1b2c0c0
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-07 19:32:26 +0919

    ans: medium-108

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,10 @@
-type Trim<S extends string> = any
+// 空白文字（スペース、改行、タブ）を表す型エイリアス
+type Space = ' ' | '\n' | '\t'
+
+// 文字列型の両端の空白文字を削除する。テンプレートリテラル型と条件型、再帰を組み合わせて実装。
+// まず左側の空白を削除し、次に右側の空白を削除する処理を再帰的に適用する。
+type Trim<S extends string> = S extends `${Space}${infer R}` 
+  ? Trim<R> 
+  : S extends `${infer L}${Space}` 
+    ? Trim<L> 
+    : S
