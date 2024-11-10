b1a4ac7d7f9ce7eb20cb6304cafabc3a52d2b608
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-10 16:28:29 +0919

    ans: medium-110

--- questions/00110-medium-capitalize/template.ts
+++ questions/00110-medium-capitalize/template.ts
@@ -1,1 +1,5 @@
-type MyCapitalize<S extends string> = any
+// 文字列の最初の文字を大文字に変換する型。テンプレートリテラル型と条件型、組み込みの Uppercase ユーティリティ型を使用。
+// 文字列を最初の文字(F)と残りの部分(R)に分割し、Fを大文字に変換してRと結合する。空文字列の場合はそのまま返す。
+type MyCapitalize<S extends string> = S extends `${infer F}${infer R}` 
+  ? `${Uppercase<F>}${R}` 
+  : S
