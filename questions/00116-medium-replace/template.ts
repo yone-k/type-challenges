af98f4a9453925298193cd9e8bbe23806e8d28f3
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-11 23:57:12 +0919

    ans: medium-116

--- questions/00116-medium-replace/template.ts
+++ questions/00116-medium-replace/template.ts
@@ -1,1 +1,10 @@
-type Replace<S extends string, From extends string, To extends string> = any
+// 文字列型内のサブストリングを別の文字列に置換する型。テンプレートリテラル型と条件型を使用して実装。
+// 置換対象のFromが空文字列の場合は何も置換せずSをそのまま返す。
+// それ以外の場合、Sを「置換前(L) + From + 置換後(R)」のパターンにマッチさせ、マッチした場合は「L + To + R」に置換する。
+// マッチしない場合はSをそのまま返す。
+type Replace<S extends string, From extends string, To extends string> = 
+  From extends '' 
+    ? S 
+    : S extends `${infer L}${From}${infer R}` 
+      ? `${L}${To}${R}` 
+      : S
