3415a0303410cfe0303de6c015750b4ddef14195
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-12 18:18:36 +0919

    ans: medium-119

--- questions/00119-medium-replaceall/template.ts
+++ questions/00119-medium-replaceall/template.ts
@@ -1,1 +1,7 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+// 文字列型内のすべてのサブストリングを別の文字列に置換する型。
+type ReplaceAll<S extends string, From extends string, To extends string> =
+  From extends ''
+    ? S
+    : S extends `${infer L}${From}${infer R}`
+      ? `${L}${To}${ReplaceAll<R, From, To>}`
+      : S
