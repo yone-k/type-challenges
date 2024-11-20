bc159aa736873f23dbdb3291805c737d53ab562f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-20 22:45:35 +0919

    ans: medium-527

--- questions/00527-medium-append-to-object/template.ts
+++ questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,7 @@
-type AppendToObject<T, U, V> = any
+// オブジェクト型に新しいプロパティを追加する型。マップド型と条件型を使用して実装。
+// マップされるキーは元のオブジェクトのキー(keyof T)と新しいキー(U)の合併型となる。
+// 条件型を使って、元のオブジェクトに存在するキーの場合は元の値を保持し、新しいキーの場合は指定された値(V)を使用する。
+// Tはオブジェクト型に制約し、Uはプロパティキーとして有効な型(string, number, symbol)に制約している。
+type AppendToObject<T extends object, U extends string | number | symbol, V> = {
+  [K in keyof T | U]: K extends keyof T ? T[K] : V
+}
