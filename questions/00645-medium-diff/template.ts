d198309b95d0d8a593ee3ca7a7d4f18fb54d24b6
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-11-29 21:06:44 +0919

    ans: medium-645

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,8 @@
-type Diff<O, O1> = any
+// 2つのオブジェクト型の差分を取得する型。片方のオブジェクトにしか存在しないプロパティのみを持つ型を生成する。
+type Diff<O, O1> = {
+  [K in Exclude<keyof O | keyof O1, keyof O & keyof O1>]: K extends keyof O 
+    ? O[K] 
+    : K extends keyof O1 
+      ? O1[K] 
+      : never
+}
