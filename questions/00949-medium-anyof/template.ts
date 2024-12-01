409073ef67445271554c7ab41d17cb5539b48dce
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-01 20:04:14 +0919

    ans: medium-949

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,11 @@
-type AnyOf<T extends readonly any[]> = any
+// 配列内の少なくとも1つの要素が真値であるかをチェックする型
+// 偽値: 0, '', false, [], {}, undefined, null
+// 真値: 他のすべての値
+type FalsyValues = 0 | '' | false | [] | Record<string, never> | undefined | null
+
+type AnyOf<T extends readonly any[]> = 
+  T extends [infer First, ...infer Rest]
+    ? First extends FalsyValues
+      ? AnyOf<Rest>
+      : true
+    : false
