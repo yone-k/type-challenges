d009aef395d90c7420dd9864caddb7ebcb379a4f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-02 18:18:06 +0919

    ans: medium-1097

--- questions/01097-medium-isunion/template.ts
+++ questions/01097-medium-isunion/template.ts
@@ -1,1 +1,10 @@
-type IsUnion<T> = any
+// 型パラメータがユニオン型かどうかをチェックする型
+// ユニオン型に対する条件型の分配特性を利用
+type IsUnion<T, U = T> = 
+  [T] extends [never] 
+    ? false 
+    : T extends U 
+      ? [U] extends [T] 
+        ? false 
+        : true 
+      : never
