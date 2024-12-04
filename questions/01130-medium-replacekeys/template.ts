4d9eb58ee39e5d14f0d0b45c2381b0f349171118
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-04 20:50:44 +0919

    ans: medium-1130

--- questions/01130-medium-replacekeys/template.ts
+++ questions/01130-medium-replacekeys/template.ts
@@ -1,1 +1,10 @@
-type ReplaceKeys<U, T, Y> = any
+// オブジェクト型の指定されたキーを、新しい値の型で置き換える型
+// T: 置き換えるキーのユニオン型
+// Y: 新しいキーと値の型を持つオブジェクト型
+type ReplaceKeys<U, T extends string | number | symbol, Y> = {
+  [K in keyof U]: K extends T
+    ? K extends keyof Y
+      ? Y[K]
+      : never
+    : U[K]
+}
