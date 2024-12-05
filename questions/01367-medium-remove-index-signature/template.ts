3801d7e68182a280b6f89618090e5a62fded1fa9
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-05 19:37:06 +0919

    ans: medium-1367

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,12 @@
-type RemoveIndexSignature<T> = any
+// オブジェクト型からインデックスシグネチャを削除する型
+// インデックスシグネチャの特性として、キーが文字列リテラル、数値リテラル、またはシンボルリテラルの場合にのみ保持する
+type RemoveIndexSignature<T> = {
+  [K in keyof T as string extends K 
+    ? never 
+    : number extends K 
+      ? never 
+      : symbol extends K 
+        ? never 
+        : K
+  ]: T[K]
+}
