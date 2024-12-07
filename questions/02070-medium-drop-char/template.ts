ffbd95702756d3a9783137abaee3235f4755c2c1
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-07 22:55:17 +0919

    ans: medium-2070

--- questions/02070-medium-drop-char/template.ts
+++ questions/02070-medium-drop-char/template.ts
@@ -1,1 +1,7 @@
-type DropChar<S, C> = any
+// 指定された文字を文字列から削除する型
+type DropChar<S extends string, C extends string> = 
+  S extends `${infer First}${infer Rest}`
+    ? First extends C
+      ? DropChar<Rest, C>
+      : `${First}${DropChar<Rest, C>}`
+    : S
