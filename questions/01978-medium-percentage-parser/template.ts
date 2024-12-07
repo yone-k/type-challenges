1771da5f241be5befbb0f57a1b8da3c3d737658f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-07 19:22:44 +0919

    ans: medium-1978

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,25 @@
-type PercentageParser<A extends string> = any
+// パーセンテージ文字列をパースして、[符号, 数値, パーセント記号]の形式のタプル型を返す型
+// 符号: '+', '-', または ''
+// 数値: 数字の文字列または ''
+// パーセント記号: '%' または ''
+
+// 符号部分を抽出する
+type ParseSign<S extends string> = S extends `${infer F}${infer R}`
+  ? F extends '+' | '-'
+    ? [F, R]
+    : ['', S]
+  : ['', ''];
+
+// パーセント記号を抽出する
+type ParsePercent<S extends string> = S extends `${infer R}%`
+  ? [R, '%']
+  : [S, ''];
+
+// 完全な分解処理
+type PercentageParser<A extends string> = A extends ''
+  ? ['', '', '']
+  : ParseSign<A> extends [infer S extends string, infer R extends string]
+    ? ParsePercent<R> extends [infer N extends string, infer P extends string]
+      ? [S, N, P]
+      : never
+    : never;
