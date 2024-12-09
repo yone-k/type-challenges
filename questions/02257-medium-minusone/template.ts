fa6a15a44909d863ea5e24cc4ce4110a8d3f910e
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-09 21:24:09 +0919

    ans: medium-2257

--- questions/02257-medium-minusone/template.ts
+++ questions/02257-medium-minusone/template.ts
@@ -1,1 +1,13 @@
-type MinusOne<T extends number> = any
+// 数値から1を引く型
+// ”型のインスタンス化は非常に深く、無限である可能性があります。” のエラー解消が難しい。小さい数値でのみ動作する。
+type BuildArray<L extends number, Arr extends unknown[] = []> =
+  Arr['length'] extends L
+    ? Arr
+    : BuildArray<L, [...Arr, unknown]>
+
+type MinusOne<T extends number> =
+  T extends 0
+    ? -1
+    : BuildArray<T> extends [...infer U, unknown]
+      ? U['length']
+      : never
