f505e60698e6eff2bfc93fbe2914b2b71fa5816f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-12 23:45:13 +0919

    ans: medium-2688

--- questions/02688-medium-startswith/template.ts
+++ questions/02688-medium-startswith/template.ts
@@ -1,1 +1,2 @@
-type StartsWith<T extends string, U extends string> = any
+// テンプレートリテラル型と条件型を使用して、文字列Tが文字列Uで始まるかどうかを判定する
+type StartsWith<T extends string, U extends string> = T extends `${U}${infer _}` ? true : false
