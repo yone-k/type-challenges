dfec8e3eb7cc7fd0c3e7abd5690375efc8241c90
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-15 14:27:25 +0919

    ans: medium-2693

--- questions/02693-medium-endswith/template.ts
+++ questions/02693-medium-endswith/template.ts
@@ -1,1 +1,2 @@
-type EndsWith<T extends string, U extends string> = any
+// テンプレートリテラル型と条件型を使用して、文字列Tが文字列Uで終わるかどうかを判定する
+type EndsWith<T extends string, U extends string> = T extends `${infer _}${U}` ? true : false
