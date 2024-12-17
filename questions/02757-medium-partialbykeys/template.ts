1f978b3c4ee9dddcdfca250e450342596cda1a7e
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-17 20:01:53 +0919

    ans: medium-2757

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,6 @@
-type PartialByKeys<T, K> = any
+// 指定されたキーのプロパティをオプショナルにする型を作成する
+type PartialByKeys<T, K extends keyof T = keyof T> = {
+  [P in keyof T as P extends K ? P : never]?: T[P]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+} extends infer O ? { [P in keyof O]: O[P] } : never
