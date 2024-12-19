99d2e0d59f9a2d0dea386911adc5c590c8840bfa
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-19 21:47:55 +0919

    ans: medium-2759

--- questions/02759-medium-requiredbykeys/template.ts
+++ questions/02759-medium-requiredbykeys/template.ts
@@ -1,1 +1,6 @@
-type RequiredByKeys<T, K> = any
+// 指定されたキーのプロパティを必須にする型を作成する
+type RequiredByKeys<T, K extends keyof T = keyof T> = {
+  [P in keyof T as P extends K ? P : never]-?: T[P]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+} extends infer O ? { [P in keyof O]: O[P] } : never
