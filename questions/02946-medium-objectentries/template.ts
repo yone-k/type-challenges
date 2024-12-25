8a9ff6d5ed2ee8a3e5cda861b9181f2222ee0b48
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-25 19:12:28 +0919

    ans: medium-2946

--- questions/02946-medium-objectentries/template.ts
+++ questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,7 @@
-type ObjectEntries<T> = any
+// Object.entriesの型版を実装
+// 各プロパティのキーと値のペアを[K, V]形式のタプル型の共用体として表現
+// Required<T>を使用してオプショナルプロパティを必須に変換し、Partial<T>でも正しく動作するようにする
+// never型をundefinedに変換して、特殊なケースも適切に処理
+type ObjectEntries<T, U = Required<T>> = {
+  [K in keyof U]: [K, U[K] extends never ? undefined : U[K]]
+}[keyof U]
