433a344bba4b1bf7af3cb9e3529d48e59ba00a52
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2024-12-30 20:23:07 +0919

    ans: medium-3196

--- questions/03196-medium-flip-arguments/template.ts
+++ questions/03196-medium-flip-arguments/template.ts
@@ -1,1 +1,6 @@
-type FlipArguments<T> = any
+// 関数の引数型タプルを取得し、型内で定義したReverseArgsを使用して逆順にした後、同じ戻り値型で新しい関数型を構築する
+type ReverseArgs<T extends any[]> = T extends [...infer Rest, infer Last] ? [Last, ...ReverseArgs<Rest>] : []
+
+type FlipArguments<T extends (...args: any[]) => any> = T extends (...args: infer Args) => infer Return
+  ? (...args: ReverseArgs<Args>) => Return
+  : never
