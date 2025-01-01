e6f580ae05955eae48c0dd337c28d7ae45b1bdcc
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-01 23:29:15 +0919

    ans: medium-3243

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -1,1 +1,12 @@
-type FlattenDepth = any
+// 配列をパラメータで指定された深さまでフラット化する型
+// Countはタプル型で深さをカウントするために使用する（型システムでの数値計算）
+type FlattenOnce<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? F extends unknown[]
+    ? [...F, ...FlattenOnce<R>]
+    : [F, ...FlattenOnce<R>]
+  : []
+
+type FlattenDepth<T extends unknown[], D extends number = 1, Count extends unknown[] = []> =
+  Count['length'] extends D
+    ? T
+    : FlattenDepth<FlattenOnce<T>, D, [...Count, unknown]>
