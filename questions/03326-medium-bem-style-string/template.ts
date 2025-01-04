b676b30c0b9fceca82ace0bb69c6519c0db189a9
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-04 21:58:16 +0919

    ans: medium-3326

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,13 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+// BEMスタイルの文字列を生成する型
+// B（ブロック）、E（要素）とM（修飾子）の配列を受け取り、BEMの命名規則に沿った文字列の型を生成する
+type BEM<
+  B extends string,
+  E extends string[],
+  M extends string[]
+> = E['length'] extends 0
+  ? M['length'] extends 0
+    ? B
+    : `${B}--${M[number]}`
+  : M['length'] extends 0
+    ? `${B}__${E[0]}`
+    : `${B}__${E[0]}--${M[number]}`
