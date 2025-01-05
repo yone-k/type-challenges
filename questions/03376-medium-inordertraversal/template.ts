8ca8ac9b4beeabd29c579db5fd1f7d99aa9fc8b8
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-05 14:52:46 +0919

    ans: medium-3376

--- questions/03376-medium-inordertraversal/template.ts
+++ questions/03376-medium-inordertraversal/template.ts
@@ -3,4 +3,14 @@ interface TreeNode {
   left: TreeNode | null
   right: TreeNode | null
 }
-type InorderTraversal<T extends TreeNode | null> = any
+
+// 二分木の中順走査（左->根->右）を実装する型
+// nullの場合は空配列、それ以外の場合は左、根、右の順で再帰的に処理して結果を結合する
+type InorderTraversal<T extends TreeNode | null> =
+  T extends TreeNode
+    ? [
+        ...(T['left'] extends TreeNode ? InorderTraversal<T['left']> : []),
+        T['val'],
+        ...(T['right'] extends TreeNode ? InorderTraversal<T['right']> : [])
+      ]
+    : []
