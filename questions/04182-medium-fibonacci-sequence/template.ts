20d6c2a433004c8f0922a95b9b6ef7b16e94a78e
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-06 22:48:03 +0919

    ans: medium-4182

--- questions/04182-medium-fibonacci-sequence/template.ts
+++ questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,15 @@
-type Fibonacci<T extends number> = any
+// フィボナッチ数列を計算する型
+// 再帰的にF(n) = F(n-1) + F(n-2)を計算し、F(1)=F(2)=1をベースケースとする
+type Fibonacci<
+  T extends number,
+  CurrentIndex extends number[] = [1],
+  Prev extends number[] = [],
+  Current extends number[] = [1]
+> = CurrentIndex['length'] extends T
+  ? Current['length']
+  : Fibonacci<
+      T,
+      [...CurrentIndex, 1],
+      Current,
+      [...Prev, ...Current]
+    >
