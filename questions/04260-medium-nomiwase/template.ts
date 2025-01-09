7d8af4fd4dc7732e2c6dba4076d073a6ab4c11af
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-09 18:08:30 +0919

    ans: medium-4260

--- questions/04260-medium-nomiwase/template.ts
+++ questions/04260-medium-nomiwase/template.ts
@@ -1,1 +1,13 @@
-type AllCombinations<S> = any
+// 文字列から文字のユニオン型を作成
+type StringToUnion<S extends string> =
+  S extends `${infer F}${infer R}`
+    ? F | StringToUnion<R>
+    : never;
+
+// 文字列の全ての組み合わせを生成
+type AllCombinations<S extends string> =
+  [S] extends [never]
+    ? ''
+    : '' | {
+        [K in StringToUnion<S>]: K | `${K}${AllCombinations<Exclude<StringToUnion<S>, K>>}`
+      }[StringToUnion<S>]
