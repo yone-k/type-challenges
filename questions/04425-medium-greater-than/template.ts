0da97521d916236361623d7f93dc5cf3a8d572ed
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-12 17:20:44 +0919

    ans: medium-4425

--- questions/04425-medium-greater-than/template.ts
+++ questions/04425-medium-greater-than/template.ts
@@ -1,1 +1,8 @@
-type GreaterThan<T extends number, U extends number> = any
+// タプルを用いて数値の大小比較を行う型
+// 各数値に対応する長さのタプルを生成し、比較する
+type GreaterThan<T extends number, U extends number, A extends any[] = []> = 
+  T extends A['length'] 
+    ? false 
+    : U extends A['length'] 
+      ? true 
+      : GreaterThan<T, U, [...A, any]>
