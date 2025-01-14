cbd78e54b637958feaacb64e5dde8223928fb62c
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-14 20:42:04 +0919

    ans: medium-4471

--- questions/04471-medium-zip/template.ts
+++ questions/04471-medium-zip/template.ts
@@ -1,1 +1,8 @@
-type Zip<T, U> = any
+// 2つのタプルの対応する要素をペアにして新しいタプルを作成する型
+// 再帰的に処理し、どちらかのタプルが空になったらそこで終了する
+type Zip<T extends any[], U extends any[]> =
+  T extends [infer TFirst, ...infer TRest]
+    ? U extends [infer UFirst, ...infer URest]
+      ? [[TFirst, UFirst], ...Zip<TRest, URest>]
+      : []
+    : []
