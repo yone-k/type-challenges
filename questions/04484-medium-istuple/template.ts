5045bf16914afab982511e3c07e099c8e16d68a1
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-15 23:32:28 +0919

    ans: medium-4484

--- questions/04484-medium-istuple/template.ts
+++ questions/04484-medium-istuple/template.ts
@@ -1,1 +1,10 @@
-type IsTuple<T> = any
+// タプル型かどうかを判定する型
+// タプル型は固定長の配列型で、配列型やオブジェクト型と区別する必要がある
+type IsTuple<T> =
+  [T] extends [never]
+    ? false
+    : T extends readonly any[]
+      ? number extends T['length']
+        ? false
+        : true
+      : false
