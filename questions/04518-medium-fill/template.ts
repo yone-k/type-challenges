8f0efb799019f65e73a665fdd64e3c1f577185ff
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-18 19:46:32 +0919

    ans: medium-4518

--- questions/04518-medium-fill/template.ts
+++ questions/04518-medium-fill/template.ts
@@ -1,6 +1,16 @@
+// 配列の指定された範囲を指定値で埋める型
+// カウンタとフラグを用いて再帰的に処理し、StartからEnd-1の範囲の要素をNで置き換える
 type Fill<
   T extends unknown[],
   N,
   Start extends number = 0,
   End extends number = T['length'],
-> = any
+  Count extends any[] = [],
+  Flag extends boolean = Count['length'] extends Start ? true : false,
+> = Count['length'] extends End
+  ? T
+  : T extends [infer R, ...infer U]
+    ? Flag extends false
+      ? [R, ...Fill<U, N, Start, End, [...Count, 0]>]
+      : [N, ...Fill<U, N, Start, End, [...Count, 0], Flag>]
+    : T
