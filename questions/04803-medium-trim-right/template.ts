cf43a2bb7706f9917c122f9c873fe7e691a4f770
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-19 16:04:13 +0919

    ans: medium-4803

--- questions/04803-medium-trim-right/template.ts
+++ questions/04803-medium-trim-right/template.ts
@@ -1,1 +1,3 @@
-type TrimRight<S extends string> = any
+// 文字列の末尾の空白文字（スペース、改行、タブ）を削除する型
+// 文字列の末尾が空白文字の場合、その部分を除いた残りの文字列にTrimRightを再帰的に適用する
+type TrimRight<S extends string> = S extends `${infer R}${' ' | '\n' | '\t'}` ? TrimRight<R> : S
