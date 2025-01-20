1f92d22033ae6ae7a1cc41ecfb80a688680d2c15
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-20 18:15:52 +0919

    ans: medium-5117

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,7 @@
-type Without<T, U> = any
+// 配列から指定された要素を除外する型
+// U が配列の場合はその要素のユニオン型に変換し、各要素を比較して除外する
+type Without<T extends unknown[], U> = T extends [infer F, ...infer R]
+  ? F extends (U extends unknown[] ? U[number] : U)
+    ? Without<R, U>
+    : [F, ...Without<R, U>]
+  : []
