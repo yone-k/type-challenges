beb70dc167d65697e4d5baaca98abac2053752a1
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-21 20:12:36 +0919

    ans: medium-5140

--- questions/05140-medium-trunc/template.ts
+++ questions/05140-medium-trunc/template.ts
@@ -1,1 +1,9 @@
-type Trunc = any
+// 数値や文字列の小数部分を切り捨てて整数部分だけを返す型
+type Trunc<T extends string | number> = 
+  `${T}` extends `${infer S}${'.'}${string}` 
+    ? S extends '' | '-'
+      ? S extends '-'
+        ? '-0'
+        : '0'
+      : S
+    : `${T}`
