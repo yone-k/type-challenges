28ce6f44406caa444d7c3ec0bef0db3a3865a28e
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-24 18:27:12 +0919

    ans: medium-5153

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,13 @@
-type IndexOf<T, U> = any
+// 配列内の指定された要素の最初のインデックスを返す型
+// 見つからない場合は-1を返す
+type IndexOf<T extends any[], U, I extends any[] = []> =
+  T extends [infer F, ...infer R]
+    ? Equal<F, U> extends true
+      ? I['length']
+      : IndexOf<R, U, [...I, 0]>
+    : -1
+
+// Equal型ヘルパー
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
