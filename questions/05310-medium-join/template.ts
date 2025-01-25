3066c5ecc348af65888d09d66e68223c04c3357a
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-25 23:09:45 +0919

    ans: medium-5310

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,9 @@
-type Join<T, U> = any
+// 配列の要素を指定した区切り文字で結合する型
+type Join<T extends unknown[], U extends string | number = ','> = 
+  T extends []
+    ? ''
+    : T extends [infer F]
+      ? `${F & string}`
+      : T extends [infer F, ...infer R]
+        ? `${F & string}${U}${Join<R, U>}`
+        : never
