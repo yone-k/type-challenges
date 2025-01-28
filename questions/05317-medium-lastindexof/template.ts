fa35b6ff2c22591dcd04535cffd13bfe347e0713
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-28 19:20:13 +0919

    ans: medium-5317

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,14 @@
-type LastIndexOf<T, U> = any
+// 配列内の指定された要素が最後に出現するインデックスを返す型
+type LastIndexOf<T extends any[], U> = 
+  T extends [...infer Rest, infer Last]
+    ? Equal<Last, U> extends true
+      ? Rest['length'] 
+      : LastIndexOf<Rest, U> extends -1
+        ? -1
+        : LastIndexOf<Rest, U>
+    : -1
+
+// Equal型ヘルパー
+type Equal<X, Y> = 
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
