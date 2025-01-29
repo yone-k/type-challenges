0cf66ef508e5993f63b047b69ce173f9ea223bc1
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-29 18:53:32 +0919

    ans: medium-5360

--- questions/05360-medium-unique/template.ts
+++ questions/05360-medium-unique/template.ts
@@ -1,1 +1,20 @@
-type Unique<T> = any
+// 配列から重複要素を取り除いたユニークな配列を返す型
+type Unique<T extends any[], Result extends any[] = []> = 
+  T extends [infer First, ...infer Rest]
+    ? Includes<Result, First> extends true
+      ? Unique<Rest, Result>
+      : Unique<Rest, [...Result, First]>
+    : Result
+
+// 配列に特定の要素が含まれているかをチェックする型
+type Includes<T extends any[], U> = 
+  T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true
+      ? true
+      : Includes<Rest, U>
+    : false
+
+// Equal型ヘルパー
+type Equal<X, Y> = 
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
