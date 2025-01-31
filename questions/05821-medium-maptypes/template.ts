97a85689ab28d9f4967b70a879337d79b8bc624f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-01-31 18:24:10 +0919

    ans: medium-5821

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,10 @@
-type MapTypes<T, R> = any
+// オブジェクトの型をマッピングルールに従って変換する型
+type MapTypes<T, R> = {
+  [K in keyof T]: T[K] extends R extends { mapFrom: infer F, mapTo: infer To } 
+    ? F extends T[K] 
+      ? R extends { mapFrom: T[K], mapTo: infer U } 
+        ? U 
+        : never 
+      : T[K] 
+    : T[K]
+}
