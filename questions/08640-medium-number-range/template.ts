dacfda4f5f313b7403f35f7a32e1ae5285f1201f
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-05 18:39:35 +0919

    ans: medium-8640

--- questions/08640-medium-number-range/template.ts
+++ questions/08640-medium-number-range/template.ts
@@ -1,1 +1,7 @@
-type NumberRange<L, H> = any
+// 指定された範囲の数値のユニオン型を生成する型
+type NumberRange<L extends number, H extends number, A extends any[] = [], Res = never> =
+  A['length'] extends L
+    ? A['length'] extends H
+      ? L | Res
+      : NumberRange<L, H, [...A, 0], A['length'] | Res>
+    : NumberRange<L, H, [...A, 0], Res>
