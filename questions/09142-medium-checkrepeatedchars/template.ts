f74dda92034b97cb8d5e49bb5ac6e6ea0b2af533
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-08 21:10:20 +0919

    ans: medium-9142

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,7 @@
-type CheckRepeatedChars<T extends string> = any
+// 文字列に重複する文字が含まれているかをチェックする型
+type CheckRepeatedChars<T extends string, Chars extends string = never> = 
+  T extends `${infer First}${infer Rest}`
+    ? First extends Chars
+      ? true
+      : CheckRepeatedChars<Rest, Chars | First>
+    : false
