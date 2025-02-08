bf2c39b065c864ee8e55440ef698771bd064f884
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-08 15:26:36 +0919

    ans: medium-8987

--- questions/08987-medium-subsequence/template.ts
+++ questions/08987-medium-subsequence/template.ts
@@ -1,1 +1,5 @@
-type Subsequence<T extends any[]> = any
+// 配列の全ての部分列を生成する型
+type Subsequence<T extends any[]> = 
+  T extends [infer F, ...infer R]
+    ? [] | [F] | [F, ...Subsequence<R>] | Subsequence<R>
+    : []
