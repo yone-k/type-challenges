2e45ed8eb8ceaa764c6a556e9f64d76876b0b2e2
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-08 13:57:25 +0919

    ans: medium-8767

--- questions/08767-medium-combination/template.ts
+++ questions/08767-medium-combination/template.ts
@@ -1,1 +1,5 @@
-type Combination<T extends string[]> = any
+// 文字列配列のすべての組み合わせを生成する型
+type Combination<T extends string[], U = T[number]> = 
+  U extends string 
+    ? U | `${U} ${Exclude<Combination<T>, U>}`
+    : never
