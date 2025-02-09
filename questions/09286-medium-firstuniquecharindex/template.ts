33cb9b8353cb4c23592b7d33567765d57fe0b6c8
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-09 21:37:57 +0919

    ans: medium-9286

--- questions/09286-medium-firstuniquecharindex/template.ts
+++ questions/09286-medium-firstuniquecharindex/template.ts
@@ -1,1 +1,16 @@
-type FirstUniqueCharIndex<T extends string> = any
+// 文字列内で最初に現れる一意の文字のインデックスを返す型
+// 文字列を先頭から走査し、既に出現した文字を_Accに記録
+// 現在の文字が_Accに含まれていない場合、残りの文字列に同じ文字が出現するかチェック
+// 出現しない場合、その文字が最初の一意の文字となる
+type FirstUniqueCharIndex<
+  T extends string,
+  _Acc extends string[] = []
+> = T extends ''
+  ? -1
+  : T extends `${infer Head}${infer Rest}`
+  ? Head extends _Acc[number]
+  ? FirstUniqueCharIndex<Rest, [..._Acc, Head]>
+  : Rest extends `${string}${Head}${string}`
+  ? FirstUniqueCharIndex<Rest, [..._Acc, Head]>
+  : _Acc['length']
+  : never
