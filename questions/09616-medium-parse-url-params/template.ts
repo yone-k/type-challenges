5d84963d1476da92fc0f258d0c2eff3f58c7b8b8
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-11 22:08:59 +0919

    ans: medium-9616

--- questions/09616-medium-parse-url-params/template.ts
+++ questions/09616-medium-parse-url-params/template.ts
@@ -1,1 +1,7 @@
-type ParseUrlParams<T> = any
+// URLパラメータ文字列からパラメータ名を抽出する型
+type ParseUrlParams<T extends string> = 
+  T extends `${string}:${infer Param}/${infer Rest}`
+    ? Param | ParseUrlParams<Rest>
+    : T extends `${string}:${infer Param}`
+      ? Param
+      : never
