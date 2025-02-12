29a08a7b43f9d37978f1538f9d176afc2964d4df
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-12 22:57:01 +0919

    ans: medium-9896

--- questions/09896-medium-get-middle-element/template.ts
+++ questions/09896-medium-get-middle-element/template.ts
@@ -1,1 +1,9 @@
-type GetMiddleElement<T> = any
+// 再帰的に配列の両端から要素を削除して中央の要素を取得する
+type GetMiddleElement<T extends unknown[]> = 
+  T extends [infer First, ...infer Middle, infer Last]
+    ? Middle extends []
+      ? [First, Last]
+      : GetMiddleElement<Middle>
+    : T extends [infer Single]
+      ? [Single]
+      : []
