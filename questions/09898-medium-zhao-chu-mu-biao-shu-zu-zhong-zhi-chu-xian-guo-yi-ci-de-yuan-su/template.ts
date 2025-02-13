add22b7cf5f0ce0c55630b892bea319f2b3d25c6
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-13 22:24:24 +0919

    ans: medium-9898

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,15 @@
-type FindEles<T extends any[]> = any
+// 配列内で一度だけ出現する要素を抽出する型
+type IncludesInUnion<U, T> = [U] extends [never]
+    ? false
+    : U extends T
+    ? true
+    : false
+
+type FindEles<
+    T extends unknown[],
+    Duplicates extends unknown[] = [],
+> = T extends [infer Head, ...infer Tail]
+    ? IncludesInUnion<Duplicates[number] | Tail[number], Head> extends false
+    ? [Head, ...FindEles<Tail, Duplicates>]
+    : FindEles<Tail, [...Duplicates, Head]>
+    : T
