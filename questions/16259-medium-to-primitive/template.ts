cd61e23f60b0f621798b9740ee3e898f862175b6
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-16 23:56:09 +0919

    ans: medium-16259

--- questions/16259-medium-to-primitive/template.ts
+++ questions/16259-medium-to-primitive/template.ts
@@ -1,1 +1,17 @@
-type ToPrimitive = any
+// リテラル型をプリミティブ型に変換する
+type ToPrimitive<T> = 
+  T extends Function
+    ? Function
+    : T extends [infer F, ...infer R]
+      ? [ToPrimitive<F>, ...ToPrimitive<R>]
+      : T extends readonly [infer E]
+        ? readonly [ToPrimitive<E>]
+        : T extends object
+          ? { [P in keyof T]: ToPrimitive<T[P]> }
+          : T extends string
+            ? string
+            : T extends number
+              ? number
+              : T extends boolean
+                ? boolean
+                : T
