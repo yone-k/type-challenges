832535563bb7df6780834fe6413d47a2f78127fe
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-16 20:43:26 +0919

    ans: medium-10969

--- questions/10969-medium-integer/template.ts
+++ questions/10969-medium-integer/template.ts
@@ -1,1 +1,8 @@
-type Integer<T> = any
+// 数値が整数かどうかを判定する
+type Integer<T extends number> = number extends T
+  ? never
+  : `${T}` extends `${infer N extends number}.${string}`
+    ? `${T}` extends `${N}.${'0'}`
+      ? Integer<N>
+      : never
+    : T
