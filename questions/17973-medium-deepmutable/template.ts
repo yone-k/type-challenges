5000a01e265e2a785d2d5c29288aec35a1b0d9c9
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-17 19:34:08 +0919

    ans: medium-17973

--- questions/17973-medium-deepmutable/template.ts
+++ questions/17973-medium-deepmutable/template.ts
@@ -1,1 +1,7 @@
-type DeepMutable = any
+// オブジェクトの全プロパティを再帰的にmutableにする
+type DeepMutable<T extends Record<keyof any, any>> =
+    T extends (...args: any[]) => any ?
+    T :
+    {
+        - readonly [K in keyof T]: DeepMutable<T[K]>
+    }
