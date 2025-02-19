356dd973e81653fa359136a804099b14abc48872
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-19 23:05:17 +0919

    ans: medium-18142

--- questions/18142-medium-all/template.ts
+++ questions/18142-medium-all/template.ts
@@ -1,1 +1,10 @@
-type All = any
+// 配列の全要素が指定した値と等しいかを判定する
+type All<T extends unknown[], U> = T extends [infer First, ...infer Rest]
+  ? Equal<First, U> extends true
+    ? All<Rest, U>
+    : false
+  : true
+
+type Equal<X, Y> = 
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
