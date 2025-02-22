34b8e07ed957c50edbc7c812baef700af580405b
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-22 22:14:56 +0919

    ans: medium-18220

--- questions/18220-medium-filter/template.ts
+++ questions/18220-medium-filter/template.ts
@@ -1,1 +1,6 @@
-type Filter<T extends any[], P> = []
+// 配列から指定した型の要素だけをフィルタリングする
+type Filter<T extends any[], P> = T extends [infer First, ...infer Rest]
+  ? [First] extends [P]
+    ? [First, ...Filter<Rest, P>]
+    : Filter<Rest, P>
+  : []
