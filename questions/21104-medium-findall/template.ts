f17153a446a02bc666ad1d805c24800310b2ddba
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-23 22:01:15 +0919

    ans: medium-21104

--- questions/21104-medium-findall/template.ts
+++ questions/21104-medium-findall/template.ts
@@ -1,1 +1,11 @@
-type FindAll<T extends string, P extends string> = any
+// 文字列内の全てのパターンの出現位置（インデックス）を取得する
+type FindAll<T extends string, P extends string, I extends number[] = [], Pos extends 1[] = []> =
+  P extends ''
+    ? []
+    : T extends ''
+      ? I
+      : T extends `${infer First}${infer Rest}`
+        ? T extends `${P}${infer _}`
+          ? FindAll<Rest, P, [...I, Pos['length']], [...Pos, 1]>
+          : FindAll<Rest, P, I, [...Pos, 1]>
+        : I
