f57c93ef2a43ac012dfac6868c28d9eda3134c4e
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-25 23:23:21 +0919

    ans: medium-21106

--- questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
+++ questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
@@ -1,2 +1,8 @@
-// 实现 Combs
-type Combs<T extends any[]> = any
+// 修飾キーの組み合わせを生成する（優先順位を保持）
+type Combs<T extends any[]> = T extends [infer F, ...infer R]
+  ? CombsHelper<F & string, R> | Combs<R>
+  : never
+
+type CombsHelper<F extends string, R extends any[]> = R extends [infer H, ...infer Rest]
+  ? `${F} ${H & string}` | CombsHelper<F, Rest>
+  : never
