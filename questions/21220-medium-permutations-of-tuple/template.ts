62b2a449db6e3db575602fd289854e9d23b7feab
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-02-27 21:15:13 +0919

    ans: medium-21220

--- questions/21220-medium-permutations-of-tuple/template.ts
+++ questions/21220-medium-permutations-of-tuple/template.ts
@@ -1,1 +1,16 @@
-type PermutationsOfTuple<T extends unknown[]> = any
+// タプルの全ての順列（置換パターン）を生成する型
+type Insert<
+    T extends unknown[],
+    U
+> =
+    T extends [infer F, ...infer L]
+    ? [F, U, ...L] | [F, ...Insert<L, U>]
+    : [U]
+
+type PermutationsOfTuple<
+    T extends unknown[],
+    R extends unknown[] = []
+> =
+    T extends [infer F, ...infer L] ?
+    PermutationsOfTuple<L, Insert<R, F> | [F, ...R]>
+    : R
