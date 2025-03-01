7e70b8520e61872e8704460316f94b395038e26c
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-01 22:28:26 +0919

    ans: medium-25170

--- questions/25170-medium-replace-first/template.ts
+++ questions/25170-medium-replace-first/template.ts
@@ -1,1 +1,6 @@
-type ReplaceFirst<T extends readonly unknown[], S, R> = any
+// タプル内の最初に出現する特定の型の要素を別の型に置き換える
+type ReplaceFirst<T extends readonly unknown[], S, R> = T extends [infer First, ...infer Rest]
+  ? First extends S
+    ? [R, ...Rest]
+    : [First, ...ReplaceFirst<Rest, S, R>]
+  : T
