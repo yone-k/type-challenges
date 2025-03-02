27c511e2b811ba733693508994f3298f05c66c84
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-02 18:22:42 +0919

    ans: medium-25270

--- questions/25270-medium-transpose/template.ts
+++ questions/25270-medium-transpose/template.ts
@@ -1,1 +1,6 @@
-type Transpose<M extends number[][]> = any
+// 行列（二次元配列）の転置を行う（行と列を入れ替える）
+type Transpose<M extends number[][], R = M['length'] extends 0 ? [] : M[0]> = {
+    [X in keyof R]: {
+        [Y in keyof M]: X extends keyof M[Y] ? M[Y][X] : never
+    }
+}
