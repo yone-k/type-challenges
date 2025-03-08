cb95fc5b49e44c7383a3bc6b2ea22b68fc814eff
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-08 22:39:14 +0919

    ans: medium-27152

--- questions/27152-medium-triangular-number/template.ts
+++ questions/27152-medium-triangular-number/template.ts
@@ -1,1 +1,2 @@
-type Triangular<N extends number> = any
+// 三角数（1からNまでの合計）を計算する
+type Triangular<N extends number, P extends number[] = [], A extends number[] = []> = P['length'] extends N ? A['length'] : Triangular<N, [0, ...P], [...A, ...P, 0]>
