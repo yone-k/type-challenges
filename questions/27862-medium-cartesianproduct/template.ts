cc1229e02a230ac064687bfc8c1df98bb211d4fb
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-09 14:46:31 +0919

    ans: medium-27862

--- questions/27862-medium-cartesianproduct/template.ts
+++ questions/27862-medium-cartesianproduct/template.ts
@@ -1,1 +1,6 @@
-type CartesianProduct<T, U> = any
+// 2つの集合（ユニオン型）のデカルト積を計算する
+type CartesianProduct<T, U> = T extends infer TItem
+  ? U extends infer UItem
+    ? [TItem, UItem]
+    : never
+  : never
