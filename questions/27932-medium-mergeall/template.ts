d74518b2e81a8232e096d8328f5600d661d47e8b
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-11 22:49:16 +0919

    ans: medium-27932

--- questions/27932-medium-mergeall/template.ts
+++ questions/27932-medium-mergeall/template.ts
@@ -1,1 +1,17 @@
-type MergeAll<XS> = any
+// 複数のオブジェクト型をマージし、重複するキーの値をユニオン型にする
+type MergeAll<XS extends object[], Result = {}> = XS extends []
+  ? Result
+  : XS extends [infer First extends object, ...infer Rest extends object[]]
+    ? MergeAll<
+        Rest,
+        {
+          [K in keyof Result | keyof First]: K extends keyof First
+            ? K extends keyof Result
+              ? Result[K] | First[K]
+              : First[K]
+            : K extends keyof Result
+              ? Result[K]
+              : never
+        }
+      >
+    : never
