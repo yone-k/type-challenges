899aed9eaa94b2e784822b9b75eea6a47b2bf1b4
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-19 20:39:26 +0919

    ans: medium-29785

--- questions/29785-medium-deep-omit/template.ts
+++ questions/29785-medium-deep-omit/template.ts
@@ -1,1 +1,12 @@
-type DeepOmit = any
+// オブジェクト型からドット記法で指定されたパスのプロパティを削除する
+type DeepOmit<T, K extends string> = K extends `${infer F}.${infer R}`
+  ? F extends keyof T
+    ? {
+        [P in keyof T]: P extends F
+          ? DeepOmit<T[P], R>
+          : T[P]
+      }
+    : T
+  : {
+      [P in keyof T as P extends K ? never : P]: T[P]
+    }
