989a031c38da54c31661d6e1781ce1c7674922b1
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-20 19:00:54 +0919

    ans: medium-30301

--- questions/30301-medium-isodd/template.ts
+++ questions/30301-medium-isodd/template.ts
@@ -1,1 +1,8 @@
-type IsOdd<T extends number> = any
+// 数値が奇数かどうかを判定する
+type IsOdd<T extends number> = number extends T
+  ? false
+  : `${T}` extends `${number}.${string}` | `${string}e${string}`
+    ? false
+    : `${T}` extends `${string}${0 | 2 | 4 | 6 | 8}`
+      ? false
+      : true
