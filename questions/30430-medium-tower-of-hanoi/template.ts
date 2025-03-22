ba8f8bf54607e768506e09b06a58bcd0bc487b43
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-22 21:56:11 +0919

    ans: medium-30430

--- questions/30430-medium-tower-of-hanoi/template.ts
+++ questions/30430-medium-tower-of-hanoi/template.ts
@@ -1,1 +1,14 @@
-type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = any
+// ハノイの塔の移動手順を再帰的に生成する型
+type Hanoi<
+    N extends number,
+    From = "A",
+    To = "B",
+    Intermediate = "C",
+    CurrentIndex extends 1[] = []
+> = CurrentIndex["length"] extends N
+    ? []
+    : [
+        ...Hanoi<N, From, Intermediate, To, [...CurrentIndex, 1]>,
+        [From, To],
+        ...Hanoi<N, Intermediate, To, From, [...CurrentIndex, 1]>
+    ]
