3e1b33685055a8f09cc07b6ba5eded644ad7aeda
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-24 23:53:03 +0919

    ans: medium-30970

--- questions/30970-medium-shitariteraru/template.ts
+++ questions/30970-medium-shitariteraru/template.ts
@@ -1,1 +1,2 @@
-type IsFixedStringLiteralType<S extends string> = any
+// 固定文字列リテラル型かどうかを判定する
+type IsFixedStringLiteralType<S extends string> = {} extends Record<S, 1> ? false : Equal<[S], S extends unknown ? [S] : never>
