02f2542afa4642f0593a41d0bf70da2e6955f4fa
Kaito Yonezu <mis.oma.yo.387@gmail.com>
2025-03-25 18:45:13 +0919

    ans: medium-34007

--- questions/34007-medium-compare-array-length/template.ts
+++ questions/34007-medium-compare-array-length/template.ts
@@ -1,1 +1,11 @@
-type CompareArrayLength<T extends any[], U extends any[]> = any
+// 2つの配列の長さを比較する
+type CompareArrayLength<T extends any[], U extends any[]> = 
+  T extends [...U, ...infer Rest1]
+    ? Rest1['length'] extends 0
+      ? 0
+      : 1
+    : U extends [...T, ...infer Rest2]
+      ? Rest2['length'] extends 0
+        ? 0
+        : -1
+      : 0
